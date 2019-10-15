3ccd962a37d8d21432f0887ce7a914bf46c03936
Thomas Klebel <t.klebel@hotmail.com>
2019-10-15 20:17:40 +0200

    drake plan

---
+++ R/02-plan.R
@@ -0,0 +1,15 @@
+plan <- drake_plan(
+  raw_data = read_csv(
+    file_in("data/results-survey896447.csv"),
+    col_types = cols(
+      .default = col_character(),
+      id = col_double(),
+      submitdate = col_datetime(format = ""),
+      lastpage = col_double(),
+      seed = col_double(),
+      `DT03[other]` = col_logical()
+    )),
+  data = raw_data %>% 
+    clean_data(),
+  rda_fig = create_rda_fig(data, file_out("figs/data_sharing.png"))
+)
