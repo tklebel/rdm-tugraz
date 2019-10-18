b92973b3156f2f5f2b7125a0381c2397843e0f65
Thomas Klebel <t.klebel@hotmail.com>
2019-10-18 13:09:04 +0200

    add some notes on how creating figs could be automatized

--- R/02-plan.R
+++ R/02-plan.R
@@ -12,5 +12,12 @@ plan <- drake_plan(
   data = raw_data %>% 
     clean_data(),
   rda_fig = create_rda_fig(data, file_out("figs/data_sharing.png")),
+  # this could then be generalized by using `crossing`, and using paste to 
+  # create out files. so we would have a single function that generalizes 
+  # `create_test_fig` to a range of figures. Problematic though: this will
+  # not allow for the necessary degree of customization. 
+  # For printing, I will probably end up with single functions per single fig.
+  # Maybe then not to spend too much time on getting this first version right, 
+  # as it is only for exploratory purposes
   test_fig = create_test_fig(data, file_out("figs/test.png"))
 )
