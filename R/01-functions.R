df850ed118cab7356fd4609b6fc55f0dec02e8a0
Thomas Klebel <t.klebel@hotmail.com>
2019-10-18 13:04:28 +0200

    create function to generate unidimensional display of groups of questions

--- R/01-functions.R
+++ R/01-functions.R
@@ -1,9 +1,4 @@
-clean_data <- function(raw_data) {
-  raw_data %>% 
-    set_names(., str_replace_all(names(.), "\\[|\\]", "_"))
-}
-
-
+# Helper functions ------
 make_proportion <- function(df, var, group, order_string = NA_character_,
                             .drop_na = FALSE) {
   counts <- df %>%
@@ -23,10 +18,68 @@ make_proportion <- function(df, var, group, order_string = NA_character_,
     )
 }
 
+make_univ_fig <- function(data, var, .drop_na, order_string,
+                         title) {
+    pdata <- data %>% 
+      pivot_longer(cols = starts_with(var),
+                   names_to = "var", values_to = "val") %>% 
+      select(var, val) 
+    
+    if (.drop_na) pdata <- drop_na(pdata)
+    
+    
+    # determine type of response category and recode accordingly
+    if (str_detect(pdata$val, "Always, or")) {
+      pdata <- pdata %>% 
+        mutate(val = factor(val, levels = c(
+          "Always, or almost always", "Most of the time", "Sometimes",
+          "Rarely", "Never, or almost never", "Do not know/cannot answer")))
+    } else if (str_detect(pdata$val, "Completely agree")) {
+      pdata <- pdata %>% 
+        mutate(val = factor(val, levels = c(
+          "Completely agree", "Tend to agree", "Tend to disagree",
+          "Completely disagree", "Do not know/cannot answer")))
+    } else if (str_detect(pdata$val, "To a ")) {
+      pdata <- pdata %>% 
+        mutate(val = factor(val, levels = c(
+          "To a very large extent", "To a large extent", "To some extent",
+          "To little or no extent at all", "Do not know/cannot answer")))
+    }
+  
+    
+    pdata <- pdata %>% 
+      make_proportion(val, var, order_string = order_string)
+    
+    pdata %>% 
+      ggplot(aes(fct_reorder(var, order), prop, fill = val)) +
+      ggchicklet::geom_chicklet(width = .7) +
+      scale_y_continuous(labels = scales::percent) +
+      scale_fill_brewer(palette = "Dark2") +
+      coord_flip() +
+      hrbrthemes::theme_ipsum(base_family = "Hind") +
+      labs(x = NULL, y = NULL, fill = NULL, 
+           title = title) +
+      theme(legend.position = "top")
+    
+  
+}
+
+
+
+
+
+# Functions for drake plan -----
+
+clean_data <- function(raw_data) {
+  raw_data %>% 
+    set_names(., str_replace_all(names(.), "\\[|\\]", "_"))
+}
+
 create_rda_fig <- function(data, out_path, width = 10, height = 5) {
   pdata <- data %>% 
     pivot_longer(cols = starts_with("DHRP05"),
                  names_to = "var", values_to = "val") %>% 
+    select(var, val) %>% 
     mutate(var_recoded = case_when(
       str_detect(var, "01") ~ "...to an institutional repository or data center (provided by TU Graz)",
       str_detect(var, "02") ~ "...to a non-institutional repository or data center (e.g. arXiv, GitHub)",
@@ -55,4 +108,13 @@ create_rda_fig <- function(data, out_path, width = 10, height = 5) {
   
   ggsave(out_path, p, width = width, height = height, dpi = 400)
 }
+
+create_test_fig <- function(data, out_path) {
+  p <- make_univ_fig(data, "DHRP05", .drop_na = T, "Sometimes", "test-title")
+  
+  ggsave(out_path, p, width = 10, height = 5, dpi = 400)
+}
+
+
+
 
