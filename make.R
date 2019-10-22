744aa6a190ca1949d89bebadd70355fd2449f125
Thomas Klebel <t.klebel@hotmail.com>
2019-10-22 12:51:02 +0200

    rearrange make script

--- make.R
+++ make.R
@@ -1,12 +1,13 @@
 source("R/00-packages.R")
 source("R/01-functions.R")
-source("R/02-plan.R")
 
+source("R/02-plan.R")
+make(plan)
 
 # config <- drake_config(plan)
 # vis_drake_graph(config)
 
-make(plan)
+
 
 
 
